9231d4334e083525699fef78686c43668bbb2ad7
john-lay <john_lay@hotmail.co.uk>
2016-06-02 13:58:50 +0100

    Update HelloWorldSection.cs

--- App_Plugins/HelloWorld/HelloWorldSection.cs
+++ App_Plugins/HelloWorld/HelloWorldSection.cs
@@ -1,14 +1,9 @@
 namespace umbraco_demo.App_Plugins.HelloWorld
 {
     using umbraco.businesslogic;
-    using umbraco.interfaces;
 
+    [Application("helloWorldSection", "Hello World", "icon-users-alt", 15)]
     public class HelloWorldSection
     {
-        [Application("helloWorldSection", "Hello World", "icon-users-alt", 15)]
-        public class LoyaltyAdminSection : IApplication
-        {
-
-        }
     }
 }
